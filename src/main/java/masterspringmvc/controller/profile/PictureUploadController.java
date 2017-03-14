765eb6e8a97de2ebf1597976399da6be9271b063
twicoder <renqingwei123@163.com>
2017-03-14 15:48:23 +0800

    add upload picture function

--- src/main/java/masterspringmvc/controller/profile/PictureUploadController.java
+++ src/main/java/masterspringmvc/controller/profile/PictureUploadController.java
@@ -1,15 +1,19 @@
 package masterspringmvc.controller.profile;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sun.nio.ch.IOUtil;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLConnection;
 
 /**
  * Created by I302636 on 3/14/2017.
@@ -24,15 +28,36 @@ public class PictureUploadController {
     }
 
     @RequestMapping(value="/upload",method= RequestMethod.POST)
-    public String onUpload(MultipartFile file) throws IOException {
-        String filename = file.getOriginalFilename();
-        File tempFile = File.createTempFile("pic",getFileExtension(filename),PICTURES_DIR.getFile());
+    public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs) throws IOException {
+        if(file.isEmpty() || !isImage(file)){
+            redirectAttrs.addFlashAttribute("error","Incorrect file. Please upload a picture.");
+            return "redirect:/upload";
+        }
 
-        try(InputStream in = file.getInputStream(); OutputStream out=new FileOutputStream(tempFile)){
+        copyFileToPictures(file);
+
+        return "profile/uploadPage";
+    }
+
+    @RequestMapping(value="/uploadedPicture")
+    public void getUploadedPicture(HttpServletResponse response) throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource("/images/anonymous.png");
+        response.setHeader("Content-Type", URLConnection.guessContentTypeFromName(classPathResource.getFilename()));
+        IOUtils.copy(classPathResource.getInputStream(),response.getOutputStream());
+    }
+
+    private Resource copyFileToPictures(MultipartFile file) throws IOException {
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        File tempFile = File.createTempFile("pic",fileExtension,PICTURES_DIR.getFile());
+        try(InputStream in = file.getInputStream();OutputStream out = new FileOutputStream(tempFile)){
             IOUtils.copy(in,out);
         }
 
-        return "profile/uploadPage";
+        return new FileSystemResource(tempFile);
+    }
+
+    private boolean isImage(MultipartFile file){
+        return file.getContentType().startsWith("image");
     }
 
     private static String getFileExtension(String name){
